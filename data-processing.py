970881552a85b68812747f66c0fcfdaa1e91b6f0
Alma Liezenga <alma.liezenga@gmail.com>
2022-12-06 19:50:41 +0100

    Created the script for creating patches out of images and added the data

---
+++ data-processing.py
@@ -0,0 +1,49 @@
+# step 1 in Documentation
+
+# import libraries 
+import pandas as pd 
+from PIL import Image
+
+#create standard dirs 
+dir_input = 'data/input/'
+dir_output = 'data/clean/'
+
+#open annotations file --> here you should put the directory to your sample annotations file 
+annotations = pd.read_csv(f'{dir_input}Annotations.csv')
+
+#check full size to be able to track progress 
+total_size = len(annotations.index)
+
+# !improvement would be to not loop through all annotations but instead all images (computationally more efficient)!
+
+#function that takes an image and point locations and returns a 100x100 pixels patch for each point with the point as the center
+def create_patch(im, x, y): 
+    #calculate bounding box for patch given coordinates 
+    left = x-50
+    upper = y-50
+    right = x+50
+    lower = y+50
+    box = (left, upper, right, lower)
+
+    #create a patch
+    subim = im.crop(box)
+    print(f'I have just created a patch for {x}, {y}')
+    return(subim)
+
+# a loop that uses the function 
+for i, r in annotations.iterrows(): 
+    #get the correct image
+    region = r['region']
+    qid = r['quadratid']
+    im = Image.open(f'{dir_input}{region}/{qid}.jpg')
+    
+    #get the coordinates of the sample 
+    subim = create_patch(im, r['x'], r['y'])
+    
+    #save the sample
+    subim.save(f"data/clean/{region}/{qid}_{x}_{y}.jpg")
+    print(f'I have just saved the patch {x}, {y} for image {qid} from {region}')
+    
+    #print progress
+    progress = (i / total_size) * 100
+    print(f'I am now at {progress} % of the annotations')
---
+++ data-sampling.py
@@ -0,0 +1,9 @@
+# step 2 in Documentation
+
+# Do a random sample of 10.000 annotations. 
+# 
+# Make sure the coral types are balanced. 
+# 
+# Use the random annotations to create patches and save those to a new folder in the drive. 
+# 
+# This will be your dataset.
